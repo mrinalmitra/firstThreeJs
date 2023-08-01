4eb2583ae29047b355cfa844cc1421cde9ab626a
personal <mrinalgithub@gmail.com>
2023-08-01 21:08:08 +0530

    chore: rotating cube

--- main.js
+++ main.js
@@ -1,28 +1,31 @@
 import * as THREE from "three";
 
-// Scene
 const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  65,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
-// Object
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
-// Sizes
-const sizes = {
-  width: 800,
-  height: 600,
-};
+camera.position.z = 6;
 
-// Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
-scene.add(camera);
+function animate() {
+  requestAnimationFrame(animate);
 
-// Renderer
-const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector("canvas.webgl"),
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
